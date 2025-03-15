79586d836b147e6511db83983f98f69ee5d48780
benson <bensonmunge545@gmail.com>
2025-03-15 13:11:40 +0300

    added new changes

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Your code goes here
+
+document.addEventListener("DOMContentLoaded", function() {
+    // Select the paragraph element with the id "text"
+    const paragraph = document.getElementById("text");
+  
+    // Modify its content to "This is really cool!"
+    paragraph.textContent = "This is really cool!";
+  });
+  
